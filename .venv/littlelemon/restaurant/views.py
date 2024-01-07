bdce7016904a2669bb52d8799fab82376dddef4d
Inoday <yadavinoday@gmail.com>
2024-01-07 08:38:24 -0500

    menu and table booking API setup

--- .venv/littlelemon/restaurant/views.py
+++ .venv/littlelemon/restaurant/views.py
@@ -1,5 +1,40 @@
+from rest_framework.decorators import api_view
+from .models import Menu, Booking
 from django.shortcuts import render
+from rest_framework import generics, viewsets, permissions
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+# Create your views here. 
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+   # def get(self, request):
+      #  return render(request, 'index.html', {})
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    #def get(self, request):
+      #  return render(request, 'index.html', {})
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    #def get(self, request):
+        #  return render(request, 'index.html', {})
+
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated]
+   
 def index(request):
     return render(request, 'index.html', {})
+    
+    
